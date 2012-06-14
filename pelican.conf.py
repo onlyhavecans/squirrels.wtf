1e0e7b9e20c037ee485dca07b5630dc1499b5e29
tBunnyMan <tbunnyman@me.com>
2012-06-13 17:57:28 -0700

    Change the URL structure to match the old site and other dynamic pages

--- pelican.conf.py
+++ pelican.conf.py
@@ -9,6 +9,25 @@ DEFAULT_LANG='en'
 DEFAULT_PAGINATION = 5
 DELETE_OUTPUT_DIRECTORY = False
 
+## I want my URLs to mimic a dynamic site; ie, they all point to a / with an index
+# Article
+ARTICLE_URL = 'post/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'post/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+ARTICLE_LANG_URL = 'post/{date:%Y}/{date:%b}/{date:%d}/{slug}-{lang}/'
+ARTICLE_LANG_SAVE_AS = '/post/{date:%Y}/{date:%b}/{date:%d}/{slug}-{lang}/index.html'
+# Page
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+PAGE_LANG_URL = '{slug}-{lang}/'
+PAGE_LANG_SAVE_AS = '{slug}-{lang}/index.html'
+# Category
+AUTHOR_URL = 'author/{name}/'
+AUTHOR_SAVE_AS = 'author/{name}/index.html'
+CATEGORY_URL = 'category/{name}/'
+CATEGORY_SAVE_AS = 'category/{name}/index.html'
+TAG_URL = 'tag/{name}/'
+TAG_SAVE_AS = 'tag/{name}/index.html'
+
 FEED_DOMAIN = SITEURL
 # Use RSS
 FEED_RSS = 'feeds/rss'
